3ddf05d1bbcc73364f88fa9d0f7f1da3686d2d0a
doveka <131176206+doveka@users.noreply.github.com>
2023-12-27 22:46:45 +0200

    Add files via upload

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string str1 = "abcdefg";
+        string str2 = "bdhijk";
+        
+        string result = GetUniqueCharacters(str1, str2);
+        
+        Console.WriteLine(result);
+    }
+    
+    static string GetUniqueCharacters(string str1, string str2)
+    {
+        string uniqueCharacters = "";
+        
+        foreach (char c in str1)
+        {
+            if (!str2.Contains(c.ToString()))
+            {
+                uniqueCharacters += c;
+            }
+        }
+        
+        return uniqueCharacters;
+    }
+}
